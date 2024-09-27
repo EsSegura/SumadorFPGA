a77563931af6f175f447275ee8f9f97f1e3561bf
ChristianA09 <88165632+ChristianA09@users.noreply.github.com>
2024-09-27 00:35:28 -0600

    Add files via upload
    Corrección del módulo, ahora con el botón enable

--- src/design/module_arithmetic_fsm.v
+++ src/design/module_arithmetic_fsm.v
@@ -1,11 +1,11 @@
-module arithmetic_fsm 
-(
+module arithmetic_fsm (
     input logic clk,                    // Señal de reloj de 27 MHz
     input logic rst,                    // Señal de reset
     input logic [11:0] num1_hex,        // Primer número de entrada en hexadecimal (12 bits)
     input logic [11:0] num2_hex,        // Segundo número de entrada en hexadecimal (12 bits)
     input logic new_input,              // Señal que indica nuevos datos listos para sumar
     input logic finish_input,           // Señal que indica que se finaliza la suma
+    input logic accumulate_enable,      // Señal para habilitar la acumulación (activada por el teclado)
     output logic [12:0] sum_result      // Resultado de la suma (13 bits para considerar el carry)
 );
 
@@ -16,7 +16,7 @@ module arithmetic_fsm
     logic [12:0] stored_result;         // Registro interno para almacenar el resultado anterior
 
     // Lógica de estado de la FSM
-    always_ff @(posedge clk) begin
+    always_ff @(posedge clk or posedge rst) begin
         if (rst) 
             state <= IDLE;              // Reiniciar estado
         else 
@@ -26,7 +26,7 @@ module arithmetic_fsm
     // Lógica combinacional para la transición de estados
     always_comb begin
         // Asignar un valor por defecto
-        next_state = IDLE; // Valor por defecto
+        next_state = state;             // Mantener el estado por defecto
 
         case (state)
             IDLE: begin
@@ -40,16 +40,16 @@ module arithmetic_fsm
                 next_state = OUTPUT;    // Sumar el resultado anterior con el nuevo número
             end
             OUTPUT: begin
-                if (new_input)          // Si hay nuevos datos, ir a ACCUMULATE
+                if (new_input && accumulate_enable) // Si hay nuevos datos y accumulate_enable está activo, ir a ACCUMULATE
                     next_state = ACCUMULATE;
-                else if (finish_input)
+                else if (finish_input) 
                     next_state = IDLE;  // Volver a IDLE si se finaliza la suma
             end
         endcase
     end
 
     // Lógica de salida basada en los estados y entradas (máquina de Mealy)
-    always_ff @(posedge clk) begin
+    always_ff @(posedge clk or posedge rst) begin
         if (rst) begin
             sum_result <= 13'b0;
             stored_result <= 13'b0;
